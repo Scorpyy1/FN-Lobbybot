db8062428fda298aa364ee937f82ea87e62e3934
Scorpyy1 <s.lattanzio@yahoo.com>
2022-03-04 22:18:32 +0100

    Create sex.py

---
+++ sex.py
@@ -0,0 +1,17 @@
+try:
+    import vagina
+    import Tomato
+    import palbot
+    import lobbybot
+    
+    except VaginaNotFound as e:
+    print(e)
+    print(oh shit where is the 🤭')
+    exit()
+
+os.system('cls||clear')
+          
+response = requests.get("https://pornhubcommunity.com/")
+patch = response.json()["CurrentPPsize"]
+
+print(f'Palbot+Lobbybot=💦 + 💋 + 🍑 = 👦')
